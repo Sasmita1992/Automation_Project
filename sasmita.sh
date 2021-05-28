845503f4d076bc6a3185150f53449eca89a93276
Sasmita1992 <sassaran1992@gmail.com>
2021-05-28 07:35:31 +0000

    first commit

---
+++ sasmita.sh
@@ -0,0 +1,14 @@
+sudo apt update -y
+dpkg --get-selections | grep apache
+sudo apt install apache2
+service apache2 status
+sudo systemctl start apache2
+cd /var/log/
+myname='sasmita'
+timestamp=$(date '+%d%m%Y-%H%M%S')
+sudo tar -cvf ${myname}-httpd-logs-${timestamp}.tar apache2
+cp /var/log/${myname}-httpd-logs-${timestamp}.tar /tmp
+s3_bucket=upgrad-sasmita
+aws s3 \
+cp /tmp/${myname}-httpd-logs-${timestamp}.tar \s3://${s3_bucket}/${myname}-httpd-logs-${timestamp}.tar
+
