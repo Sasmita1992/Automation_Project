2165b81cf2ad419cca3f5ef563b9bf98a758ea99
root <root@ip-172-31-26-249.ec2.internal>
2021-06-02 05:25:48 +0000

    Final Code for submission

--- automation.sh
+++ automation.sh
@@ -13,39 +13,30 @@ sudo systemctl start apache2
 cd /var/log/
 myname='sasmita'
 timestamp=$(date '+%d%m%Y-%H%M%S')
-sudo tar -cvf ${myname}-httpd-logs-${timestamp}.tar apache2 
+sudo tar -cvf ${myname}-httpd-logs-${timestamp}.tar --exclude=other_vhosts_access.log  apache2/*.log 
 cp /var/log/${myname}-httpd-logs-${timestamp}.tar /tmp 
 s3_bucket=upgrad-sasmita
 aws s3 \
 cp /tmp/${myname}-httpd-logs-${timestamp}.tar \s3://${s3_bucket}/${myname}-httpd-logs-${timestamp}.tar
 #Creating Inventory.html iand apend all the data every time#
-#cd /var/log
-size=$(du -h "${myname}-httpd-logs-${timestamp}.tar" | cut -f1)
-FILE=/var/www/html/inventory.html
-if [ -f "$FILE" ]; then
-    echo "$FILE exists."
-   # myfilesize=$(stat --format=%s "/var/www/html/inventory.html")
-   # myfilesize=$(find "$size" -printf "%s")
-    cd /var/www/html
-    value=$(<inventory.html)
-    sudo echo  "$value<p>-httpd-log&emsp;&emsp;&emsp;$timestamp&emsp;&emsp;&emsp;tar&emsp;&emsp;&emsp;$size</p>"  > /var/www/html/inventory.html
-    
-else 
-    echo "$FILE is getting created."
-    cd /var/www/html
-    sudo touch /var/www/html/inventory.html
-    sudo echo "<h3>Log Type&emsp;&emsp;Time Created&emsp;&emsp;Type&emsp;&emsp;Size</h3>"  > /var/www/html/inventory.html
+size=$( (du -h /tmp/${myname}-httpd-logs-${timestamp}.tar | cut -f1) )
+FILE='/var/www/html/inventory.html'
+if [ ! -e $FILE ]; then
    
+   sudo touch /var/www/html/inventory.html
+
+   sudo echo "<h3>Log Type&emsp;&emsp;Time Created&emsp;&emsp;Type&emsp;&emsp;Size</h3>"  > /var/www/html/inventory.html
+ 
+   sudo echo  "$value<p>-httpd-log&emsp;&emsp;&emsp;$timestamp&emsp;&emsp;&emsp;tar&emsp;&emsp;&emsp;$size</p>" >> /var/www/html/inventory.html
+   cd /root/Automation_Project 
+else 
+    sudo echo "<p>-httpd-log&emsp;&emsp;&emsp;$timestamp&emsp;&emsp;&emsp;tar&emsp;&emsp;&emsp;$size</p>"  >> /var/www/html/inventory.html
 fi
 #Submit Cron job#
-FILE1=/etc/cron.d/automation1
-if [ -f "$FILE1" ]; then
-	echo "Cron job is already scheduled for tomorrow"
-else
-	
-	echo "Scheduling a cron job"
-	cd /etc/cron.d/
-	sudo touch /etc/cron.d/automation1
-	sudo echo "0 14 * * * /root  /root/Automation_Project/automation.sh" > /etc/cron.d/automation1
+FILE1=/etc/cron.d/automation
+if [ ! -e $FILE1 ]; then
+
+        cd /etc/cron.d/
+	sudo touch /etc/cron.d/automation
+	sudo echo "* * * * * root /root/Automation_Project/automation.sh" > /etc/cron.d/automation
 fi
-	
